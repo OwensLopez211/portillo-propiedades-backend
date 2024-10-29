bcfffdb8db370a68a4c2da3acc6d4fbc7aa455f2
Owens <owenslopez211@gmail.com>
2024-10-29 11:39:24 -0300

    validando que todo este al dia

--- mercadolibre_integration/urls.py
+++ mercadolibre_integration/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import get_token, unlink_mercadolibre, get_mercadolibre_items
+from .views import get_token, unlink_mercadolibre, get_mercadolibre_items, sync_mercadolibre_properties
 
 urlpatterns = [
     path('get-token/', get_token, name='get_token'),  # URL para obtener el token
     path('mercadolibre/unlink', unlink_mercadolibre, name='unlink_mercadolibre'),
     path('mercadolibre/items/', get_mercadolibre_items, name='get-mercadolibre-items'),
+    path('mercadolibre/sync/', sync_mercadolibre_properties, name='sync-mercadolibre-properties'),  # Añadir esta línea
+
 ]
