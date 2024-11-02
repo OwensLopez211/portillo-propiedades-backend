9b2aef3ac2e1091b345b21926040ef583894e3c5
Owens <owenslopez211@gmail.com>
2024-11-01 23:49:29 -0300

    configurando servicio de correos

--- contact/forms.py
+++ contact/forms.py
@@ -1,9 +1,2 @@
 # contact/forms.py
 from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Nombre')
-    lastname = forms.CharField(max_length=100, label='Apellidos')
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(max_length=15, required=False, label='Teléfono')
-    message = forms.CharField(widget=forms.Textarea, label='Comentario')
--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,13 @@
 from django.core.mail import send_mail
-from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from django.http import JsonResponse
 from django.conf import settings
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])  # Permitir acceso público
 def send_email(request):
     data = request.data
     try:
