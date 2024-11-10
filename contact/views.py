8ce2262721e030e97e22240aebac8447eb522440
Owens <owenslopez211@gmail.com>
2024-11-10 20:40:49 -0300

    Cambios .env y settings

--- contact/views.py
+++ contact/views.py
@@ -8,24 +8,24 @@ from django.conf import settings
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def enviar_correo_api(request):
-    print("Contenido de request.data:", request.data)  # Agrega esta línea
+    print("Contenido de request.data:", request.data)  # Imprime para depuración
 
     data = request.data
     nombre = data.get('nombre')
     email = data.get('email')
     mensaje = data.get('mensaje')
-    asunto = data.get('asunto', 'Nuevo mensaje')
+    asunto = data.get('asunto', f'Nuevo mensaje de {nombre}')  # Usa el asunto proporcionado o crea uno predeterminado
 
-
+    # Verifica que todos los campos necesarios estén presentes
     if not nombre or not email or not mensaje:
         return JsonResponse({'error': 'Todos los campos son requeridos'}, status=status.HTTP_400_BAD_REQUEST)
 
-    asunto = f'Nuevo mensaje de {nombre}'
+    # Construye el mensaje del correo
     mensaje_correo = f'Nombre: {nombre}\nCorreo: {email}\n\nMensaje:\n{mensaje}'
 
     try:
         send_mail(
-            asunto,
+            asunto,  # Usa el asunto proporcionado o el predeterminado
             mensaje_correo,
             settings.DEFAULT_FROM_EMAIL,
             [settings.TARGET_EMAIL],
