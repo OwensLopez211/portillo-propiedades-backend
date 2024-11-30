225fabe3b47ef7e0b75c7448135e4686228ea9af
Owens <owenslopez211@gmail.com>
2024-11-30 02:36:16 -0300

    Commit: cambios

--- portillo_propiedades_backend/settings.py
+++ portillo_propiedades_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Configuración básica
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'False'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Configuración de aplicaciones
